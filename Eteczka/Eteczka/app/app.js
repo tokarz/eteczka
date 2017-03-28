1486d659da6f9f6997b36cf9cca4bbc77d2e64c9
tokarz <mt@bahntechnik.de>
2017-03-28 10:15:33 +0200

    Poprawka burqinowego widoku :)

--- Eteczka/Eteczka/app/app.js
+++ Eteczka/Eteczka/app/app.js
@@ -45,7 +45,9 @@ app.config(function ($stateProvider) {
     var addUsersState = {
         url: '/addUsers',
         name: 'addUsers',
-        template: '<addUsers-view></addUsers-view>'
+        //Tagi rozrozniaja male-duze znaki dlatego addUsers-view nie zostalo przez angulara rozpoznane.
+        //angular automatycznie mapuje dyrektywe <jakasFajnaNazwa>' na <jakas-fajna-nazwa>
+        template: '<add-users-view></add-users-view>'
     };
     
 
