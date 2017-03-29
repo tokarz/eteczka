75f6c503c1526948a301ea705f44bb3f915e70f5
aleksandra tokarz <tokarz.aleksandra@gmail.com>
2017-03-29 17:40:10 +0200

    dodane opcje w menu uzytkownik

--- Eteczka/Eteczka/app/main/header/menu/menuController.js
+++ Eteczka/Eteczka/app/main/header/menu/menuController.js
@@ -4,4 +4,32 @@ angular.module('et.controllers').controller('menuController', ['$scope', '$state
         return $state.current.name !== 'login';
     }
 
+    $scope.userMenuVisible = false;
+
+    $scope.userMenuOptions = [
+        {
+            name: "Wyloguj",
+            iconClass: "glyphicon glyphicon-log-out",
+            link: "#"
+        },
+        {
+            name: "Edytuj Profil",
+            iconClass: "glyphicon glyphicon-pencil",
+            link: "#"
+        },
+        {
+            name: "Wyslij wiadomosc",
+            iconClass: "glyphicon glyphicon-envelope",
+            link: "#"
+        }
+        ];
+
+    $scope.showUserOptions = function () {
+        $scope.userMenuVisible = !$scope.userMenuVisible;
+    }
+
+    $scope.executeOption = function (option) {
+        //implement me
+    }
+
 }]);
