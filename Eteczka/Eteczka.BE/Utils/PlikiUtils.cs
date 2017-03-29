d5a3ac85fd0327687a5ad8bb9a4ab1e470864a2e
tokarz <mt@bahntechnik.de>
2017-03-29 22:17:42 +0200

    Dodana klasa testowa dla Michala

---
+++ Eteczka/Eteczka.BE/Utils/PlikiUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eteczka.BE.Utils
+{
+    public class PlikiUtils
+    {
+        //Przykladowe dzialanie metody:
+        //Uruchomiona z argumentem c:/katalog/plik.txt
+        //Powinna zwrocic plik.txt
+        //Jesli sciezka nie ma na koncu zadnego pliku (np c:\costam) powinna zwrocic pusty string ""
+
+        //Przydatne podpowiedzi:
+        //Metody : string.split, string lastIndexOf
+        //Testy sa juz napisane i swieca sie na czerwono!:)
+        public string WezNazwePlikuZeSciezki(string sciezka)
+        {
+
+            return "";
+        }
+    }
+}
---
+++ Eteczka/Eteczka.Be.Tests/Utils/PlikiUtilsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace Eteczka.BE.Utils
+{
+    [TestFixture]
+    public class PlikiUtilsTest
+    {
+
+        private PlikiUtils _Sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _Sut = new PlikiUtils();
+        }
+
+
+        [Test]
+        public void GetPath()
+        {
+            Assert.AreEqual("plik.txt", _Sut.WezNazwePlikuZeSciezki("c:\\dupa\\plik.txt"));
+            Assert.AreEqual("plik.txt", _Sut.WezNazwePlikuZeSciezki("c:/dupa/plik.txt"));
+            Assert.AreEqual("", _Sut.WezNazwePlikuZeSciezki("c:\\dupa"));
+            Assert.AreEqual("", _Sut.WezNazwePlikuZeSciezki(""));
+        }
+
+    }
+}
