e58f8f236d71284156111c4db687e31c6a64a467
tokarz <mt@bahntechnik.de>
2017-04-04 22:17:18 +0200

    Test Nowego koloru + D3js (biblioteka do wykresow i grafiki)

--- Eteczka/Eteczka/app/views/files/filesViewController.js
+++ Eteczka/Eteczka/app/views/files/filesViewController.js
@@ -1,24 +1,45 @@
 ﻿'use strict';
 angular.module('et.controllers').controller('filesViewController', ['$scope', '$state', 'httpService', function ($scope, $state, httpService) {
     $scope.files = [
-        { date: '2011-11-11', name: 'FolienMisko.pdf', type: 'Szkolenie' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
-         { date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' }
+        { id: 0, date: '2011-11-11', name: 'FolienMisko.pdf', type: 'Szkolenie' },
+         { id: 1, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 2, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 3, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 4, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 5, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 5, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 6, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 7, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 8, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 8, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 8, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' },
+         { id: 9, date: '2011-11-12', name: 'Tiguan.pdf', type: 'Oferta' }
     ];
+
+    $scope.allUsersForFile = {
+        '0': { date: '1410-01-01', name: 'test', type: 'X' },
+        '1': { date: '1410-01-02', name: 'test', type: 'X' },
+        '2': { date: '1410-01-03', name: 'test', type: 'X' },
+        '3': { date: '1410-01-04', name: 'test', type: 'X' },
+        '4': { date: '1410-01-05', name: 'test', type: 'X' },
+        '5': { date: '1410-01-06', name: 'test', type: 'X' },
+        '6': { date: '1410-01-07', name: 'test', type: 'X' },
+        '7': { date: '1410-01-08', name: 'test', type: 'X' },
+        '8': { date: '1410-01-09', name: 'test', type: 'X' },
+        '9': { date: '1410-01-10', name: 'test', type: 'X' }
+    }
+
+    $scope.fillInUserData = function (user) {
+
+    }
+
+    $scope.usersForFile = [];
 
     //$('#pdfPreviewer').attr('src', 'Content/img/logo9.png');
 
     $scope.previewPdf = function (elm) {
+        $scope.usersForFile = [$scope.allUsersForFile[elm.id]];
+
         $('#pdfPreviewer').addClass('processing');
         httpService.get('Resources/GetRestrictedResource', {
             fileName: elm.name
--- Eteczka/Eteczka/app/views/files/filesViewDirective.js
+++ Eteczka/Eteczka/app/views/files/filesViewDirective.js
@@ -4,7 +4,74 @@ angular.module('et.directives').directive('filesView', function () {
         restrict: 'E',
         scope: {},
         controller: 'filesViewController',
-        templateUrl: 'app/views/files/filesView.html'
+        templateUrl: 'app/views/files/filesView.html',
+        link: function (scope) {
+
+            scope.randomData = function () {
+                var diskUsageData = [
+                { label: 'Umowy', value: 25, color: '#3366CC' },
+                { label: 'Szkolenia', value: 12, color: '#DC3912' },
+                { label: 'BHP', value: 50, color: '#FF9900' },
+                { label: 'Certyfikaty', value: 13, color: '#109618' }
+                ];
+                return diskUsageData;
+            };
+
+            scope.pie = new d3pie("diskUsage", {
+                "size": {
+                    "canvasHeight": 200,
+                    "canvasWidth": 320,
+                    "pieInnerRadius": "33%",
+                    "pieOuterRadius": "100%"
+                },
+                "data": {
+                    "sortOrder": "value-desc",
+                    "content": scope.randomData()
+                },
+                "labels": {
+                    "outer": {
+                        "pieDistance": 32
+                    },
+                    "inner": {
+                        "hideWhenLessThanPercentage": 3
+                    },
+                    "mainLabel": {
+                        "fontSize": 11
+                    },
+                    "percentage": {
+                        "color": "#ffffff",
+                        "decimalPlaces": 0
+                    },
+                    "value": {
+                        "color": "#adadad",
+                        "fontSize": 11
+                    },
+                    "lines": {
+                        "enabled": true
+                    },
+                    "truncation": {
+                        "enabled": true
+                    }
+                },
+                //doesnt work on IE! patch will be released soon
+                "tooltips": {
+                    "enabled": false
+                },
+                'effects': {
+                    'pullOutSegmentOnClick': {
+                        'effect': 'linear',
+                        'speed': 400,
+                        'size': 8
+                    }
+                },
+                'misc': {
+                    'gradient': {
+                        'enabled': true,
+                        'percentage': 100
+                    }
+                }
+            });
+        }
     }
 
 });
