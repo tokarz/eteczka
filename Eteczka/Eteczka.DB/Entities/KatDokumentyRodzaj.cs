c13b2952be703452635372670c799bb79b0f3bc1
tokarz <mt@bahntechnik.de>
2017-04-22 23:40:33 +0200

    Dodalem definicje Tabel Paszczakowych do C# klas

---
+++ Eteczka/Eteczka.DB/Entities/KatDokumentyRodzaj.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eteczka.DB.Entities
+{
+    public class KatDokumentyRodzaj
+    {
+        public long Id { get; set; }//numeric(10,0) NOT NULL, -- Numer id
+        public string Symbol { get; set; } //character(20), -- Symbol dokumetu, np. SWPR - œwiadectwo pracy
+        public string Nazwa { get; set; }//character(254), -- Nazwa dokumentu
+    }
+}
