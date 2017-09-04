700cbf9dbddad41e2079532bf07835fc7bf5086f
aleksandra tokarz <tokarz.aleksandra@gmail.com>
2017-09-04 22:07:55 +0200

    powrot do bootstrapowych modali

--- Eteczka/Eteczka/app/main/components/menu-content/menuContentController.js
+++ Eteczka/Eteczka/app/main/components/menu-content/menuContentController.js
@@ -1,8 +1,6 @@
 ﻿'use strict';
 angular.module('et.controllers').controller('menuContentController', ['$scope', 'menuContentService', 'modalService', function ($scope, menuContentService, modalService) {
 
-    
-
     $scope.$watch('user', function (value) {
         console.log('watching user', value)
         if (value && value !== {}) {
@@ -21,6 +19,13 @@ angular.module('et.controllers').controller('menuContentController', ['$scope', 
         }
         console.log('should display employee dialog')
 
-        modalService.openModal(modalOptions, 'default-modal-body')
+        // modalService.openModal(modalOptions, 'default-modal-body')
+
+        modalService.showModal(modalOptions, $scope.elementSelected).then(function (result) {
+            // do wywyołania funkcja z serwisu pracownika - edytujPracownika
+            console.log(result)
+        }).catch(function (error) {
+            console.log("error found!");
+        });
     } 
 }]);
