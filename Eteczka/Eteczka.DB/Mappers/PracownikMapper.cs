6df093e058bc1979adb32acecaaf94a161f4f03c
mskalacki <mskalacki@gmai.com>
2017-09-14 13:17:39 +0200

    Poprawione mapowanie pola confidential w PracownikMapper.

--- Eteczka/Eteczka.DB/Mappers/PracownikMapper.cs
+++ Eteczka/Eteczka.DB/Mappers/PracownikMapper.cs
@@ -18,25 +18,29 @@ namespace Eteczka.DB.Mappers
             fetchedUser.PESEL = row[2].ToString();
             fetchedUser.Numeread = row[3].ToString();
             fetchedUser.Kraj = row[4].ToString();
-            fetchedUser.NazwiskoRodowe= row[5].ToString();
+            fetchedUser.NazwiskoRodowe = row[5].ToString();
             fetchedUser.ImieMatki = row[6].ToString();
-            fetchedUser.ImieOjca= row[7].ToString();
-            fetchedUser.PeselInny= row[8].ToString();
+            fetchedUser.ImieOjca = row[7].ToString();
+            fetchedUser.PeselInny = row[8].ToString();
             fetchedUser.IdOper = row[9].ToString();
-            fetchedUser.IdAkcept= row[10].ToString();
-            fetchedUser.DataModify= DateTime.Parse(row[11].ToString());
-            fetchedUser.DataAkcept= DateTime.Parse(row[12].ToString());
+            fetchedUser.IdAkcept = row[10].ToString();
+            fetchedUser.DataModify = DateTime.Parse(row[11].ToString());
+            fetchedUser.DataAkcept = DateTime.Parse(row[12].ToString());
             fetchedUser.DataUrodzenia = row[13].ToString();
             fetchedUser.Imie2 = row[14].ToString();
             fetchedUser.SystemBazowy = row[15].ToString();
             fetchedUser.Usuniety = bool.Parse(row[16].ToString());
             fetchedUser.Kodkierownik = row[17].ToString();
-            fetchedUser.Confidential = int.Parse(row [18].ToString());
+            int parsedInt = -1;
+            if (int.TryParse(row[18].ToString(), out parsedInt))
+            {
+                fetchedUser.Confidential = parsedInt;
+            }
 
             return fetchedUser;
         }
-       
-            
+
+
     }
     
 }
