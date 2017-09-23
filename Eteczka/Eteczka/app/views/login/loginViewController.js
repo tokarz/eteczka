118108cc6c505ec224a60545c4be88c9bffc18ed
aleksandra tokarz <tokarz.aleksandra@gmail.com>
2017-09-23 12:23:53 +0200

    modal administratora

--- Eteczka/Eteczka/app/views/login/loginViewController.js
+++ Eteczka/Eteczka/app/views/login/loginViewController.js
@@ -58,27 +58,41 @@ angular.module('et.controllers').controller('loginViewController', ['$rootScope'
     $scope.contactAdmin = function () {
         var modalOptions = {
             title: 'Kontakt z Adminem',
-            body: 'app/views/login/contactAdmin/contactAdminForm.html'
+            body: 'app/views/login/contactAdmin/contactAdminForm.html',
+            topicChoises: ['Stworz/Przypomnij haslo', 'Stworz/Przypomnij login']
         };
 
-        modalService.showModal(modalOptions).then(function (result) {
-            loginService.sendMessageToAdmin(result).then(function (success) {
-                if (success) {
-                    //show message - sent
-                }
-            });
-        }).catch(function (error) {
-            $mdDialog.show(
-                $mdDialog.alert()
-                   .clickOutsideToClose(true)
-                   .title('Blad Formularza')
-                   .textContent('Blad Serwera! Skontaktuj sie z Administratorem lub sprobuj ponownie za kilka minut')
-                   .ariaLabel('Alert Dialog Demo')
-                   .ok('Rozumiem')
+        modalService.showModal(modalOptions)
+            .then(function (result) {
+                // dodaj funkcje wyslania wiadomosci
+                
+                $mdDialog.show(
+                    $mdDialog.confirm()
+                        .clickOutsideToClose(true)
+                        .title('potwierdzenie')
+                        .textContent('wiadomosc przeslana do administatora')
+                        .ok('OK')
                 ).then(function () {
                     $state.go('login');
                 });
-        });
+            })
+            .catch(function (error) {
+                console.log(error)
+                if (error === 'cancel' || error === 'backdrop click') {
+                    return;
+                }
+
+                $mdDialog.show(
+                    $mdDialog.alert()
+                       .clickOutsideToClose(true)
+                       .title('Blad Formularza')
+                       .textContent('Blad Serwera! Skontaktuj sie z Administratorem lub sprobuj ponownie za kilka minut')
+                       .ariaLabel('Alert Dialog Demo')
+                       .ok('Rozumiem')
+                    ).then(function () {
+                        $state.go('login');
+                    });
+            });
     };
 
 
