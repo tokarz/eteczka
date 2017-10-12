5a6795228e7dc43b50f968d94cc82af9f7fa884c
tokarz <mt@bahntechnik.de>
2017-10-12 13:53:02 +0200

    Poprawki w skrypcie (bledna sciezka poprawiona)

--- Eteczka/Eteczka/build.bat
+++ Eteczka/Eteczka/build.bat
@@ -3,4 +3,4 @@ set SERVER_CONFIGURATION=Release
 set PUBLISH_PROFILE = "%SRC_DIR%\Properites\PublishProfiles\ETeczka.pubxml"
 @REM ------------------------------
 
-"C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe" "%SRC_DIR%\Eteczka.csproj" /p:Configuration=%SERVER_CONFIGURATION% /p:Platform=AnyCPU /clp:ErrorsOnly /t:Clean,Rebuild /p:PublishProfile=%PUBLISH_PROFILE%
+"C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe" "%SRC_DIR%\Eteczka.csproj" /p:Configuration=%SERVER_CONFIGURATION% /p:Platform=AnyCPU /clp:ErrorsOnly /t:Clean,Rebuild
