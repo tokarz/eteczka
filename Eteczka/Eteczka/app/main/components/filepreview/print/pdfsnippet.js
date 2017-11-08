938496e5cf8a7b1b66ed171ce9e7a3f96ef42b2c
tokarz <mt@bahntechnik.de>
2017-11-08 22:18:12 +0100

    Dodalem poprawiona jakosc druku pdf'a i text pustego elementu w podgladzie pliku !!:)) jakosc - zyleta!!!

--- Eteczka/Eteczka/app/main/components/filepreview/print/pdfsnippet.js
+++ Eteczka/Eteczka/app/main/components/filepreview/print/pdfsnippet.js
@@ -1,10 +1,17 @@
 ﻿function renderPDF(canvasContainer, pdfData, options) {
-    var options = options || { scale: 1 };
+    var options = options || { scale: 4 };
 
     function renderPage(page) {
 
         var viewport = page.getViewport(options.scale);
         var canvas = document.createElement('canvas');
+        var DPI = 72; // increase to improve quality
+        var PRINT_OUTPUT_SCALE = DPI / 72;
+        canvas.width = Math.floor(viewport.width * PRINT_OUTPUT_SCALE);
+        canvas.height = Math.floor(viewport.height * PRINT_OUTPUT_SCALE);
+
+        // The rendered size of the canvas, relative to the size of canvasWrapper.
+        canvas.style.width = '100%';
         var ctx = canvas.getContext('2d');
         var renderContext = {
             canvasContext: ctx,
@@ -25,8 +32,11 @@
     PDFJS.workerSrc = 'Scripts/pdfjs/workers/worker.js';
     PDFJS.getDocument({ data: pdfData }).then(function (pdf) {
         renderPages(pdf);
+
         window.postMessage("PDF_READY", "*");
     }, function (err) {
         alert(err);
     });
 }
+
+
