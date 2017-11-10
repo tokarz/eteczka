f11a4c37b70ba641f4ad24aacf9797b46e8f89a3
aleksandra tokarz <tokarz.aleksandra@gmail.com>
2017-11-10 17:29:58 +0100

    dodalam String.prototype.startsWith dla IE

--- Eteczka/Eteczka/Scripts/prototypes.js
+++ Eteczka/Eteczka/Scripts/prototypes.js
@@ -23,3 +23,12 @@ if (!Array.prototype.find) {
 } else {
     console.log('Array.prototype.find implementation exists!');
 }
+
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position) {
+        position = position || 0;
+        return this.indexOf(searchString, position) === position;
+    };
+} else {
+    console.log('String.prototype.startsWith implementation exists!');
+}
