348be8d13ea584a247c172bf5f3fc54f5b61f7c3
mskalacki <mskalacki@gmail.com>
2019-04-29 00:01:54 +0200

    Pierwszy test do FirmyDAO

---
+++ Eteczka/Eteczka.DB.Test/DAO/KatFirmyDAOTest.cs
@@ -0,0 +1,72 @@
+﻿using Eteczka.DB.Connection;
+using Eteczka.DB.DAO;
+using Eteczka.DB.Mappers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using Eteczka.Model.Entities;
+using System.Data;
+
+namespace Eteczka.DB.Tests.DAO
+{
+    [TestFixture]
+    public class KatFirmyDAOTest
+    {
+        private FirmyDAO _sut;
+
+        private IDbConnectionFactory _ConnectionFactory;
+        private IFirmyMapper _FirmyMapper;
+        private IConnection _Connection;
+        private IConnectionState _ConnectionState;
+
+        [SetUp]
+        public void Setup()
+        {
+            _ConnectionFactory = Substitute.For<IDbConnectionFactory>();
+            _Connection = Substitute.For<IConnection>();
+            _FirmyMapper = Substitute.For<IFirmyMapper>();
+            _ConnectionState = Substitute.For<IConnectionState>();
+
+            _sut = new FirmyDAO(_ConnectionFactory, _FirmyMapper, _Connection);
+
+        }
+
+        [Test]
+        public void WyszukajFirmePoNipieTest()
+        {
+
+            KatFirmy firmaZDB = new KatFirmy();
+            
+
+            string nip = "0123456789";
+            string sqlQuery = "SELECT * FROM \"KatFirmy\" WHERE nip = '" + nip + "'";
+
+            DataTable table = new DataTable();
+
+            DataRow row = table.NewRow();
+            table.Rows.Add(row);
+            
+
+            _ConnectionFactory.CreateConnectionToDB(_Connection).Returns(_ConnectionState);
+            _ConnectionState.ExecuteQuery(sqlQuery).Returns(table);
+            _FirmyMapper.MapujZSql(row).Returns(firmaZDB);
+
+            KatFirmy znalezionaFirma = _sut.WyszukajFirmePoNipie("0123456789");
+
+            Assert.AreEqual(firmaZDB, znalezionaFirma);
+            Assert.AreSame(firmaZDB, znalezionaFirma);
+
+            _ConnectionFactory.Received(1).CreateConnectionToDB(_Connection);
+            _ConnectionState.Received(1).ExecuteQuery(sqlQuery);
+            _FirmyMapper.Received(1).MapujZSql(row);
+
+        }
+    }
+
+
+   
+}
--- Eteczka/Eteczka.DB.Test/DAO/KatLoginDAOTest.cs
+++ Eteczka/Eteczka.DB.Test/DAO/KatLoginDAOTest.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 using System.Data;
 using Eteczka.DB.Utils;
 using System.Collections.Generic;
+using Eteczka.Model.DTO;
 
 namespace Eteczka.DB.DAO
 {
@@ -19,6 +20,7 @@ namespace Eteczka.DB.DAO
         private IConnectionState _ConnectionState;
         private IConnection _Connection;
         private CryptoUtils _Crypto;
+        private IKatLoginyMapper _KatLoginyMapper;
 
         [SetUp]
         public void setUp()
@@ -28,6 +30,7 @@ namespace Eteczka.DB.DAO
             _ConnectionState = Substitute.For<IConnectionState>();
             _Connection = Substitute.For<IConnection>();
             _Crypto = Substitute.For<CryptoUtils>();
+            _KatLoginyMapper = Substitute.For<IKatLoginyMapper>();
 
             _Sut = new KatLoginDAO(_ConnectionFactory, _Mapper, _Connection, _Crypto);
         }
@@ -36,7 +39,7 @@ namespace Eteczka.DB.DAO
         public void WczytajPracownikaPoNazwieIHasle()
         {
             KatLoginy someResult = new KatLoginy();
-            string sqlQuery = "SELECT * from \"KatLoginy\" WHERE identyfikator = 'someUser' and haslolong = 'HASHED_PASSWORD';";
+            string sqlQuery = "SELECT * from \"KatLoginy\" WHERE identyfikator = 'someUser' and haslolong = 'HASHED_PASSWORD' AND usuniety = false;";
 
             DataTable queryResult = Substitute.For<DataTable>();
 
@@ -52,27 +55,68 @@ namespace Eteczka.DB.DAO
             _ConnectionFactory.ReceivedWithAnyArgs().CreateConnectionToDB(null);
             _ConnectionState.Received().ExecuteQuery(sqlQuery);
             _Mapper.Received().Map(queryResult);
+
         }
 
         [Test]
         public void WczytajDetaleDlaUzytkownika()
         {
-            List<KatLoginyDetale> oczekiwanaLista = new List<KatLoginyDetale>();
+            KatLoginyDetale oczekiwaneDetale = new KatLoginyDetale();
             DataTable queryResult = Substitute.For<DataTable>();
 
 
             string sqlQuery = "SELECT * from \"KatLoginyDetale\" WHERE identyfikator = 'jakiesId';";
             _ConnectionFactory.CreateConnectionToDB(_Connection).Returns(_ConnectionState);
             _ConnectionState.ExecuteQuery(sqlQuery).Returns(queryResult);
-            _Mapper.MapDetails(queryResult).Returns(oczekiwanaLista);
+            _Mapper.MapSingleDetail(queryResult).Returns(oczekiwaneDetale);
 
             KatLoginyDetale result = _Sut.WczytajDetaleDlaUzytkownika("jakiesId");
             Assert.IsNotNull(result);
-            Assert.AreSame(oczekiwanaLista, result);
+            Assert.AreSame(oczekiwaneDetale, result);
 
             _ConnectionFactory.Received(1).CreateConnectionToDB(_Connection);
             _ConnectionState.Received(1).ExecuteQuery(sqlQuery);
-            _Mapper.Received(1).MapDetails(queryResult);
+            _Mapper.Received(1).MapSingleDetail(queryResult);
+        }
+
+        [Test]
+        public void WczytajUzytkownika()
+        {
+            AddKatLoginyDto expectedUser = new AddKatLoginyDto();
+            KatLoginyDetale detale = new KatLoginyDetale()
+            {
+                Identyfikator = "Ochodz",
+                Nazwisko = "Ochódzka",
+                Imie = "Ryszarda",
+                Email = "email@jakisemail.com"
+
+            };
+            KatLoginy loginy = new KatLoginy()
+            {
+                Identyfikator = "Ochodz",
+                Usuniety = false
+            };
+            string id = "JakiesId";
+            bool isAdmin = false;
+
+            string updateString = "SELECT * from \"KatLoginy\" WHERE identyfikator = '" + id + "' and isAdmin = '" + isAdmin + "';";
+            DataTable queryResult = Substitute.For<DataTable>();
+
+            _ConnectionFactory.CreateConnectionToDB(_Connection).Returns(_ConnectionState);
+            _ConnectionState.ExecuteQuery(updateString).Returns(queryResult);
+            _KatLoginyMapper.MapSingleDetail(queryResult).Returns(detale);
+            _KatLoginyMapper.Map(queryResult).Returns(loginy);
+
+            AddKatLoginyDto result = _Sut.WczytajUzytkownika(id, isAdmin);
+
+            Assert.IsNull(result);
+            //Assert.AreEqual(expectedUser, result);
+
+            _ConnectionFactory.Received(1).CreateConnectionToDB(_Connection);
+            _ConnectionState.Received(1).ExecuteQuery(updateString);
+            _KatLoginyMapper.Received(1).MapSingleDetail(queryResult);
+            _KatLoginyMapper.Received(1).Map(queryResult);
+
         }
 
     }
